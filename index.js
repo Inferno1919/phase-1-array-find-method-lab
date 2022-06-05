a879af765960630d823e1a6ffc9fa02b7e8eda0f
Jake <jakespeitel19@gmail.com>
2022-06-05 16:57:57 -0400

    commit tree b5860f985d3220ce244396c8ff672b8486cd7dc2

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// code your solution here
+
+
+
+function superbowlWin(array){
+    
+    let win = array.find(array => array.result === "W");
+
+  if(typeof win == 'undefined'){
+        return win;
+  }else{
+      return win.year;
+  }
+  
+
+}
